465e61351acd4fbcc9103ccb7db79ba7a09a2043
Ives M. Watanabe <ivesmw@gmail.com>
2022-11-15 17:08:30 -0300

    feat: add group handlers

--- src/infra/whatsapp-web.js/handlers/message.handler.ts
+++ src/infra/whatsapp-web.js/handlers/message.handler.ts
@@ -1,9 +1,11 @@
 import { Client } from "whatsapp-web.js";
+
 import {
   aninhaMessageHandler,
   marcusMessageHandler,
   meMessageHandler,
 } from "../messages/contacts";
+import { olarHandler } from "../messages/groups";
 import { WaWebJsMessageHelper } from "../messages/helpers";
 
 function messageHandlerFactory(client: Client) {
@@ -15,21 +17,39 @@ function messageHandlerFactory(client: Client) {
 
     if (msg.fromMe) {
       meMessageHandler(messageHelper);
-      return;
     }
 
-    const contacts = {
-      ["Marcus Vinicius Batista Siqueira"]: () =>
-        marcusMessageHandler(messageHelper),
+    const msgChat = await msg.getChat();
+    const msgFromGroup = msgChat.isGroup;
 
-      ["Baldez"]: () => aninhaMessageHandler(messageHelper),
-    };
+    if (!msgFromGroup) {
+      const contacts = {
+        ["Baldez"]: () => aninhaMessageHandler(messageHelper),
+        ["Marcus Vinicius Batista Siqueira"]: () =>
+          marcusMessageHandler(messageHelper),
+      };
 
-    const contactName = (await msg.getContact()).name;
-    const contactHandler = contacts[contactName as keyof typeof contacts];
+      const contactName = (await msg.getContact()).name;
+      const contactHandler = contacts[contactName as keyof typeof contacts];
 
-    if (contactName && contactHandler) {
-      contactHandler();
+      if (contactName && contactHandler) {
+        contactHandler();
+      }
+    } else {
+      const [, groupId] = msgChat.id._serialized.split("-");
+      const groups = [
+        {
+          id: "1490453181@g.us",
+          name: "Olar",
+          handler: olarHandler,
+        },
+      ];
+
+      const group = groups.find((group) => group.id === groupId);
+
+      if (group) {
+        group.handler(messageHelper);
+      }
     }
   });
 }
